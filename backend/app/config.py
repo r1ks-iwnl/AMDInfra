539c16e043ce7c2711b4ae0eda837e25cd66d367
r1ks-iwnl <69760936+r1ks-iwnl@users.noreply.github.com>
2026-07-09 20:30:18 +0300

    Google OAuth integration

--- backend/app/config.py
+++ backend/app/config.py
@@ -10,13 +10,21 @@ env_path = os.path.join(project_root, ".env")
 class Settings(BaseSettings):
     DATABASE_URL: str
     FRONTEND_ORIGIN: str = "http://localhost:5173"
+    GOOGLE_CLIENT_ID: str
+    GOOGLE_CLIENT_SECRET: str
+    JWT_SECRET: str
+    ALLOWED_DOMAINS: str
 
-    max_upload_bytes: int = 10 * 1024 * 1024 #10MB
+    max_upload_bytes: int = 5 * 1024 * 1024 #5MB
 
     model_config = SettingsConfigDict(
         env_file=env_path, 
         env_file_encoding="utf-8"
     )
+    
+    @property
+    def allowed_domains_list(self) -> list[str]:
+        return [d.strip() for d in self.ALLOWED_DOMAINS.split(",") if d.strip()]
 
 @lru_cache
 def get_settings():
---
+++ backend/app/routers/auth.py
@@ -0,0 +1,49 @@
+from fastapi import APIRouter, HTTPException
+from authlib.integrations.starlette_client import OAuth
+from starlette.requests import Request
+
+from ..services.auth_service import authenticate_with_google
+from ..config import get_settings
+
+settings = get_settings()
+router = APIRouter(tags=["Auth"])
+
+oauth = OAuth()
+oauth.register(
+    name="google",
+    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
+    client_id=settings.GOOGLE_CLIENT_ID,
+    client_secret=settings.GOOGLE_CLIENT_SECRET,
+    client_kwargs={"scope" : "openid email profile"}
+)
+
+@router.get("/login")
+async def login(request: Request):
+    redirect_uri = request.url_for("callback")
+    return await oauth.google.authorize_redirect(request, redirect_uri)
+
+@router.get("/callback", include_in_schema=False)
+@router.post("/callback", name="callback_post", summary="Procesează login-ul OAuth din Swagger")
+async def callback(request: Request):
+    is_post = request.method == "POST"
+    data = await request.form() if is_post else request.query_params
+    
+    if not (code := data.get("code")):
+        raise HTTPException(status_code=400, detail="Authorization code missing.")
+
+    redirect_uri = data.get("redirect_uri") or (
+        "http://localhost:8000/docs/oauth2-redirect" if is_post else str(request.url_for("callback"))
+    )
+
+    try:
+        jwt_token = await authenticate_with_google(
+            code=code, 
+            redirect_uri=redirect_uri, 
+            code_verifier=data.get("code_verifier")
+        )
+        return {"access_token": jwt_token, "token_type": "bearer"}
+        
+    except ValueError as exc:
+        raise HTTPException(status_code=400, detail=str(exc))
+    except PermissionError as exc:
+        raise HTTPException(status_code=403, detail=str(exc))
