1d7b2466ad542deb0dc6163de2f2ab87401d7c18
r1ks-iwnl <69760936+r1ks-iwnl@users.noreply.github.com>
2026-07-09 20:38:26 +0300

    use a real value

--- backend/app/schemas/bin.py
+++ backend/app/schemas/bin.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, ConfigDict, Field
 
 class BinOut(BaseModel):
     name: str = Field(..., examples=["vec[10]"], description="Name of the coverage bin")
-    value: str | None = Field(None, examples=["1'b1", "0:15"], description="Specific digital value")
+    value: str | None = Field(None, examples=["1101"], description="Specific digital value")
     hits: int = Field(..., examples=[8], description="Number of times the bin was hit during simulation")
     hit: bool = Field(..., examples=[True], description="Flag indicating if the bin was hit at least once")
     
